26d76b1b1d712afd0e325acc76b530c40f772424
Filipe Conde <filipecondep@gmail.com>
2025-11-07 09:35:07 -0300

    feat: checks if the params are numeric (needs to implement check for intmax and intmin).

--- check_input_params.c
+++ check_input_params.c
@@ -6,16 +6,25 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 20:15:40 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/11/02 21:25:48 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/11/07 09:33:33 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// static int	check_integer()
-// {
-// 	// code
-// }
+static int	check_integer(const char *str)
+{
+	// check for intmax and intmin limits
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str)
+	{
+		if (ft_isdigit(*str) == 0)
+			return (0);
+		str++;
+	}
+	return (1);
+}
 
 // static int	check_double()
 // {
@@ -31,7 +40,7 @@ int	check_input_params(char **str)
 {
 	while (*str)
 	{
-		ft_printf("%s\n", *str);
+		ft_printf("val: %d\n", check_integer(*str));
 		str++;
 	}
 	// while (*str)
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 20:31:34 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/11/02 20:55:36 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/11/07 09:34:18 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ int	main(int ac, char *av[])
 {
 	if (ac < 2)
 		return (1);
-	check_input_params(av);
+	check_input_params(++av);  // starts out by the second param cuz first is program name
 	return (0);
 }
