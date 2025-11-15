f06789549c72e565289bd89a4b0adb121cf5224e
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-11-15 15:09:57 -0300

    feat: create script for quick tests.

---
+++ quick_test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
+
+echo "=====| test start |====="
+echo "==> do full cleanup"
+make fclean
+
+echo "==> rebuild project"
+make
+
+echo -e "${GREEN}${BOLD}==> run test cases${NC}${NORMAL}"
+
+echo -e "${GREEN}${BOLD}TC01 - all valid params${NC}${NORMAL}"
+./push_swap 3 -1 0 35 400 | while IFS= read -r line; do
+    echo -e "${GREEN}${BOLD}${line}${NC}${NORMAL}";
+done
+echo
+
+echo -e "${GREEN}${BOLD}TC02 - VVIV${NC}${NORMAL}"
+./push_swap 3 -1 a 35 | while IFS= read -r line; do
+    echo -e "${GREEN}${BOLD}${line}${NC}${NORMAL}";
+done
+echo
+
+echo "==> do full cleanup"
+make fclean
+
+echo "=====|  test end  |====="
