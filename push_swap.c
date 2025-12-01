07342d5f896c1fcf29d907fd70366c0230951117
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-12-01 20:12:36 -0300

    fix: rename file and set up project for quick tests.

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 20:31:34 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/11/19 17:03:23 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/12/01 20:11:36 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,12 @@ int	main(int ac, char *av[])
 {
 	if (ac < 2)
 		return (1);
-	ft_printf("Val: %d\n", check_input_params(++av));  // starts out by the second param cuz first is program name
-	// while (++av)
-	// {
-	// 	ft_printf("val: %d\n", ft_atol(*av));
-	// }
+	// ft_printf("Val: %d\n", check_input_params(++av));  // starts out by the second param cuz first is program name
+	check_input_params(++av);
+	while (++av)
+	{
+		ft_printf("val: %d\n", ft_atol(*av));
+	}
 	
 	return (0);
 }
