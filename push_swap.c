3d35379e8d74b241c6a8757089db770d754b9b47
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-12-04 20:32:00 -0300

    feat: mount stack_a correctly.

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 20:31:34 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/12/01 22:01:02 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/12/04 20:22:47 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,23 @@ int	main(int ac, char *av[])
 	t_list	*temp;
 	int		i;
 
-	i = 1;
+	i = 0;
 	stack_a = ft_calloc(1, sizeof(t_list *));
 	node = NULL;
-	temp = *stack_a;
 	if (ac < 2)
-		return (1);
+	return (1);
 	// ft_printf("Val: %d\n", check_input_params(++av));  // starts out by the second param cuz first is program name
 	check_input_params(++av);
 	while (av[i])
 	{
-		// ft_printf("val: %d\n", ft_atol(*av));
 		node = create_el_stack_a(ft_atoi(av[i]));
-		ft_lstadd_front(stack_a, node);
+		ft_lstadd_back(stack_a, node);
 		i++;
 	}
+	temp = *stack_a;
 	while (temp)
 	{
-		ft_printf("node val: %d", *((int*)temp->content));
+		ft_printf("node val: %d\n", *((int*)temp->content));
 		temp = temp->next;
 	}
 	return (0);
