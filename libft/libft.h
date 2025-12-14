3a9f0b8f416dacb34355ced4e42bc0c90d3a0b85
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-12-14 10:20:34 -0300

    feat: create ft_lstfirst() to get the first node of a doubly linked list.

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:30:31 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/11/12 08:08:41 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/12/14 10:17:22 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,6 +71,7 @@ t_list		*ft_lstnew(void *content);
 void		ft_lstadd_front(t_list **lst, t_list *new);
 int			ft_lstsize(t_list *lst);
 t_list		*ft_lstlast(t_list *lst);
+t_list		*ft_lstfirst(t_list *lst);
 void		ft_lstadd_back(t_list **lst, t_list *new);
 void		ft_lstdelone(t_list *lst, void (*del)(void *));
 void		ft_lstclear(t_list **lst, void (*del)(void *));
