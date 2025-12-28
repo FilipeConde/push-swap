b387d2e98bcbf16b64c2bbf57bf754937c2d6213
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-12-28 12:39:47 -0300

    feat: optimize cases for four numbers list.

--- sort_short_list.c
+++ sort_short_list.c
@@ -6,11 +6,38 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/28 10:25:59 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/12/28 11:30:34 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/12/28 12:35:14 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static void	isolate_rank(t_list **a, t_list **b, int rank)
+{
+	if (*(int *)(*a)->content == rank)
+	{
+		push_b(a, b);
+		return ;
+	}
+	else if (*(int *)(*a)->next->content == rank)
+	{
+		swap_a(a);
+		push_b(a, b);
+		return ;
+	}
+	else if (*(int *)(*a)->next->next->content == rank)
+	{
+		rotate_a(a);
+		swap_a(a);
+		push_b(a, b);
+		return ;
+	}
+	else
+	{
+		reverse_rotate_a(a);
+		isolate_rank(a, b, rank);
+	}
+}
 
 static void	three_el(t_list **a)
 {
@@ -30,7 +57,14 @@ static void	three_el(t_list **a)
 	}
 	else if (*(int *)(*a)->content == 1 && *(int *)(*a)->next->content == 0 )
 		swap_a(a);
-	exit(0);
+}
+
+static void	four_el(t_list **a, t_list **b)
+{
+	isolate_rank(a, b, 3);
+	three_el(a);
+	push_a(a, b);
+	rotate_a(a);
 }
 
 void	sort_short_list(t_list **a, t_list **b)
@@ -46,6 +80,8 @@ void	sort_short_list(t_list **a, t_list **b)
 	}
 	if (size == 3)
 		three_el(a);
+	if (size == 4)
+		four_el(a, b);
 	if (check_if_sorted(a) == 0)
 	{
 		full_clear(a, b);
